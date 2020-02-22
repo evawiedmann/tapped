8519f1a992d53eaeefea995607062ed935f5e402
Eva Wiedmann <evawiedmann@gmail.com>
2020-02-21 16:41:55 -0800

    error404 added

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
     <Switch>
     <Route exact path='/' component={KegList} />
     <Route path='/newkeg' component={NewKeg} />
+    <Route component={Error404} />
     </Switch>
     </div>
   );
